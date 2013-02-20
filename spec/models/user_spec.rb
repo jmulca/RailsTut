be59971fd72cbd7499f3b1f5d881a5e426199e0b
Jason Mulcahy <jason.mulcahy@redtailtechnology.com>
2013-02-20 12:37:35 -0800

    added downcase email test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -92,4 +92,14 @@ describe User do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
   end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) {"Foo@ExaMple.CoM"}
+
+    it "should save as all lower case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
 end
