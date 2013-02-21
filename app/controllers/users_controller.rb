46607a17a586c89daba1b76739dc9d156ae558cc
Jason Mulcahy <jason.mulcahy@redtailtechnology.com>
2013-02-21 10:23:11 -0800

    added users index and rspec tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update]
   before_filter :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
 
   # GET /users/new
   # GET /users/new.json
@@ -57,7 +61,10 @@ class UsersController < ApplicationController
 
   private
     def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
     end
 
     def correct_user
