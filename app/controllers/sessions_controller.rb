71b9a30590aa5007944d095a7505d2716753f384
Jason Mulcahy <jason.mulcahy@redtailtechnology.com>
2013-02-20 17:10:21 -0800

    added user edit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,12 @@
 class SessionsController < ApplicationController
+
+  # GET /sessions/new
+  # GET /sessions/new.json
   def new
   end
 
+  # POST /sessions
+  # POST /sessions.json
   def create
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
@@ -14,6 +19,8 @@ class SessionsController < ApplicationController
     end
   end
 
+  # DELETE /sessions/1
+  # DELETE /sessions/1.json
   def destroy
     sign_out
     redirect_to root_url
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,4 +34,22 @@ class UsersController < ApplicationController
       end
     end
   end
+
+  # GET /users/1/edit
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  # PUT /users/1
+  # PUT /users/1.json
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 end
